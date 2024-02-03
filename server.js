5a9d56550dba1808793bea0da8533e1534b1f00a
Richard Lemieux <richard.lemieux@gmail.com>
2024-02-03 16:00:10 -0600

    start script in package.json

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
 import * as express from 'express';
 import * as fs from 'fs';
 import * as path from 'path';
-import * as dotenv from 'dotenv';
 import * as bodyParser from 'body-parser';
-dotenv.config();
 const app = express();
-const port = process.env.PORT || 3010;
+const port = process.env.PORT || 3000;
 // app.use(app.router);
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/body/:filename', (req, res) => {
     const filename = req.params.filename;
-    const filepath = `${__dirname}/../public/${filename}`;
+    const filepath = `${__dirname}/public/${filename}`;
     const content = fs.readFileSync(filepath).toString();
-    console.log(`==>server.ts:21 content`, content);
     res.end(content.toString());
 });
 app.get('/', (req, res) => {
-    res.send('Add a /filename to path for file download OR /body for body content');
+    res.send('Add a /filename to path for file download OR /body for body content ');
 });
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
--- server.ts
+++ server.ts
@@ -2,28 +2,24 @@ import * as express from 'express'
 import type { Express, Request, Response } from 'express'
 import * as fs from 'fs'
 import * as path from 'path'
-import * as dotenv from 'dotenv'
 import * as bodyParser from 'body-parser'
 
- dotenv.config();
-
 const app: Express = express();
-const port = process.env.PORT || 3010;
+const port = process.env.PORT || 3000;
 
 // app.use(app.router);
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/body/:filename', (req: Request, res: Response) => {
   const filename = req.params.filename
-  const filepath = `${__dirname}/../public/${filename}`
+  const filepath = `${__dirname}/public/${filename}`
   const content = fs.readFileSync(filepath).toString()
-  console.log(`==>server.ts:21 content`, content)
   res.end(content.toString())
 });
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Add a /filename to path for file download OR /body for body content');
+  res.send('Add a /filename to path for file download OR /body for body content ');
 });
 
 app.listen(port, () => {
