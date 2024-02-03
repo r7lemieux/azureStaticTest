4511a0ef4c698eee55a70bb92ef1e9597e040c8a
Richard Lemieux <richard.lemieux@gmail.com>
2024-02-03 14:53:11 -0600

    initial

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import * as express from 'express'
+import type { Express, Request, Response } from 'express'
+import * as fs from 'fs'
+import * as path from 'path'
+import * as dotenv from 'dotenv'
+import * as bodyParser from 'body-parser'
+
+ dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3010;
+
+// app.use(app.router);
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.get('/body/:filename', (req: Request, res: Response) => {
+  const filename = req.params.filename
+  const filepath = `${__dirname}/../public/${filename}`
+  const content = fs.readFileSync(filepath).toString()
+  console.log(`==>server.ts:21 content`, content)
+  res.end(content.toString())
+});
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Add a /filename to path for file download OR /body for body content');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
